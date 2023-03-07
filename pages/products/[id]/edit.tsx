a45e0d04ab1fb9cf67b67c4c16996def05bbabe6
h-genie <ses2000hj@hanmail.net>
2023-03-07 21:51:03 +0900

    2-2

---
+++ pages/products/[id]/edit.tsx
@@ -0,0 +1,141 @@
+import Carousel from 'nuka-carousel/lib/carousel'
+import Image from 'next/image'
+import { useState, useEffect } from 'react'
+import CustomEditor from '@/components/Editor'
+import { useRouter } from 'next/router'
+import { convertFromRaw, convertToRaw, EditorState } from 'draft-js'
+
+const images = [
+  {
+    original: 'https://picsum.photos/id/1018/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1018/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1015/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1015/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1016/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1016/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1013/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1013/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1012/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1012/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1011/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1011/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1019/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1019/250/150/',
+  },
+  {
+    original:
+      'https://raw.githubusercontent.com/xiaolin/react-image-gallery/master/static/4v.jpg',
+    thumbnail:
+      'https://raw.githubusercontent.com/xiaolin/react-image-gallery/master/static/4v.jpg',
+  },
+  {
+    original:
+      'https://raw.githubusercontent.com/xiaolin/react-image-gallery/master/static/3.jpg',
+    thumbnail:
+      'https://raw.githubusercontent.com/xiaolin/react-image-gallery/master/static/3.jpg',
+  },
+  {
+    original:
+      'https://raw.githubusercontent.com/xiaolin/react-image-gallery/master/static/2.jpg',
+    thumbnail:
+      'https://raw.githubusercontent.com/xiaolin/react-image-gallery/master/static/2.jpg',
+  },
+  {
+    original:
+      'https://raw.githubusercontent.com/xiaolin/react-image-gallery/master/static/1.jpg',
+    thumbnail:
+      'https://raw.githubusercontent.com/xiaolin/react-image-gallery/master/static/1.jpg',
+  },
+]
+
+export default function Products() {
+  const [index, setIndex] = useState(0)
+  const router = useRouter()
+  const { id: productId } = router.query
+  const [editorState, setEditorState] = useState<EditorState | undefined>(
+    undefined
+  )
+
+  const handleSave = () => {
+    if (editorState) {
+      fetch(`/api/update-product`, {
+        method: 'post',
+        body: JSON.stringify({
+          id: productId,
+          contents: JSON.stringify(
+            convertToRaw(editorState.getCurrentContent())
+          ),
+        }),
+      })
+        .then((res) => res.json())
+        .then(() => alert('Success'))
+    }
+  }
+
+  useEffect(() => {
+    if (productId !== undefined) {
+      fetch(`/api/get-product?id=${productId}`)
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.items.contents) {
+            setEditorState(
+              EditorState.createWithContent(
+                convertFromRaw(JSON.parse(data.items.contents))
+              )
+            )
+          } else {
+            setEditorState(EditorState.createEmpty())
+          }
+        })
+    }
+  }, [productId])
+
+  return (
+    <>
+      <Carousel
+        animation="fade"
+        autoplay
+        withoutControls
+        wrapAround
+        speed={10}
+        slideIndex={index}
+      >
+        {images.map((item, index) => (
+          <Image
+            key={index}
+            src={item.original}
+            alt=""
+            width={1000}
+            height={600}
+          />
+        ))}
+      </Carousel>
+      <div className="flex">
+        {images.map((item, index) => (
+          <div key={index} onClick={() => setIndex(index)}>
+            <Image src={item.original} alt="" width={100} height={60} />
+          </div>
+        ))}
+      </div>
+      {editorState !== undefined && (
+        <CustomEditor
+          editorState={editorState}
+          onEditorStateChange={setEditorState}
+          onSave={handleSave}
+        />
+      )}
+    </>
+  )
+}
