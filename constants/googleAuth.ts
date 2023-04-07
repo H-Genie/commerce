b63494eeb404f603b8d109fc9baefbc8543162a1
h-genie <ses2000hj@hanmail.net>
2023-04-07 22:29:55 +0900

    7-1

---
+++ constants/googleAuth.ts
@@ -0,0 +1,3 @@
+export const CLIENT_ID =
+  '63879365460-55r7uiolgppk8l2lpgf8efv3kn68oa4m.apps.googleusercontent.com'
+export const SCRET = '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'
---
+++ pages/auth/google.tsx
@@ -0,0 +1,22 @@
+import { GoogleLogin } from '@react-oauth/google'
+
+const Google = () => {
+  return (
+    <div className="flex">
+      <GoogleLogin
+        onSuccess={(credentialResponse) => {
+          fetch(
+            `/api/auth/get-token?credential=${credentialResponse.credential}`
+          )
+            .then((res) => res.json())
+            .then((data) => console.log(data))
+        }}
+        onError={() => {
+          console.log('Login Failed')
+        }}
+      />
+    </div>
+  )
+}
+
+export default Google
