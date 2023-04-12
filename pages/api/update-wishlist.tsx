cc06eb9048f70758cb4a2ba83ec8e15b3ace2e44
h-genie <ses2000hj@hanmail.net>
2023-04-12 23:57:52 +0900

    9-2

---
+++ pages/api/update-wishlist.tsx
@@ -0,0 +1,68 @@
+import { PrismaClient } from '@prisma/client'
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { unstable_getServerSession } from 'next-auth'
+import { authOptions } from './auth/[...nextauth]'
+
+const prisma = new PrismaClient()
+
+async function updateWishlist(userId: string, productId: string) {
+  try {
+    const wishlist = await prisma.wishlist.findUnique({
+      where: {
+        userId: userId,
+      },
+    })
+
+    const originWishlist =
+      wishlist?.productIds != null && wishlist.productIds != ''
+        ? wishlist.productIds.split(',')
+        : []
+    const isWished = originWishlist.includes(productId)
+
+    const newWishlist = isWished
+      ? originWishlist.filter((id) => id !== productId)
+      : [...originWishlist, productId]
+
+    const res = await prisma.wishlist.upsert({
+      where: {
+        userId,
+      },
+      update: {
+        productIds: newWishlist.join(','),
+      },
+      create: {
+        userId,
+        productIds: newWishlist.join(','),
+      },
+    })
+    console.log(res)
+    return res.productIds.split(',')
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+type Data = {
+  items?: any
+  message: string
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const session = await unstable_getServerSession(req, res, authOptions)
+  const { productId } = JSON.parse(req.body)
+
+  if (session == null) {
+    res.status(200).json({ items: [], message: 'no Session' })
+    return
+  }
+
+  try {
+    const wishlist = await updateWishlist(String(session.id), String(productId))
+    res.status(200).json({ items: wishlist, message: 'Success' })
+  } catch (error) {
+    res.status(200).json({ message: 'Failed' })
+  }
+}
--- pages/products/[id]/index.tsx
+++ pages/products/[id]/index.tsx
@@ -1,6 +1,6 @@
 import Carousel from 'nuka-carousel/lib/carousel'
 import Image from 'next/image'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import CustomEditor from '@/components/Editor'
 import { useRouter } from 'next/router'
 import { convertFromRaw, EditorState } from 'draft-js'
@@ -8,6 +8,10 @@ import { GetServerSidePropsContext } from 'next'
 import { products } from '@prisma/client'
 import { format } from 'date-fns'
 import { CATEGORY_MAP } from '@/constants/products'
+import { useMutation, useQuery } from '@tanstack/react-query'
+import { Button } from '@mantine/core'
+import { IconHeart, IconHeartbeat } from '@tabler/icons-react'
+import { useSession } from 'next-auth/react'
 
 export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
   const product = await fetch(
@@ -23,10 +27,13 @@ export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
   }
 }
 
+const WISHLIST_QUERY_KEY = '/api/get-wishlist'
+
 export default function Products(props: {
   product: products & { images: string[] }
 }) {
   const [index, setIndex] = useState(0)
+  const { data: session } = useSession()
   const router = useRouter()
   const { id: productId } = router.query
   const [editorState] = useState<EditorState | undefined>(() =>
@@ -37,7 +44,25 @@ export default function Products(props: {
       : EditorState.createEmpty()
   )
 
+  const { data: wishlist } = useQuery([WISHLIST_QUERY_KEY], () =>
+    fetch(WISHLIST_QUERY_KEY)
+      .then((res) => res.json())
+      .then((data) => data.items)
+  )
+  console.log(wishlist)
+
+  const { mutate } = useMutation<unknown, unknown, string, any>((productId) =>
+    fetch('/api/update-wishlist', {
+      method: 'POST',
+      body: JSON.stringify({ productId }),
+    })
+      .then((res) => res.json())
+      .then((data) => data.items)
+  )
+
   const product = props.product
+
+  const isWished = wishlist ? wishlist.includes(productId) : false
 
   return (
     <>
@@ -75,6 +100,32 @@ export default function Products(props: {
             <div className="text-lg">
               {product.price.toLocaleString('ko-kr')}원
             </div>
+            <>{wishlist}</>
+            <Button
+              leftIcon={
+                isWished ? (
+                  <IconHeartbeat size={20} stroke={1.5} />
+                ) : (
+                  <IconHeart size={20} stroke={1.5} />
+                )
+              }
+              style={{ backgroundColor: isWished ? 'red' : 'grey' }}
+              radius="xl"
+              size="md"
+              styles={{
+                root: { paddingRight: 14, height: 48 },
+              }}
+              onClick={() => {
+                if (session === null) {
+                  alert('로그인 해주세요')
+                  router.push('/auth/login')
+                  return
+                }
+                mutate(String(productId))
+              }}
+            >
+              찜하기
+            </Button>
             <div className="text-sm text-zinc-300">
               등록 : {format(new Date(product.createdAt), 'yyyy-MM-dd')}
             </div>
